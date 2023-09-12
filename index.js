df7c7c72ec004c792b54afb978075efd4649c614
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2023-09-12 09:43:05 +0530

    In line 1, we added a listening event to the button which listens for a button click. Then in line 2, we can obtain the number the user has typed into the <input> element.

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById('button').addEventListener('click', function() {
+    let input = document.getElementById('input').value;
+    let output = document.getElementById('output');
+  let sequence = [];
+  })
